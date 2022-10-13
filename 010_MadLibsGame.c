9085393af77611559c0ea62b85d5e94967751440
lagyer <int3rested91@gmail.com>
2022-10-13 09:15:28 +0200

    Fix2

--- 010_MadLibsGame.c
+++ 010_MadLibsGame.c
@@ -2,25 +2,25 @@
 
 int main()
 {
-	char color[20];
-    	char pluralNoun[20];
-    	char celebrityF[20];
-    	char celebrityL[20];
+    char color[20];
+    char pluralNoun[20];
+    char celebrityF[20];
+    char celebrityL[20];
     
-    	printf("\nEnter a color: ");
-	    scanf("%s", color);
-    	printf("\nEnter a plural noun: ");
-    	scanf("%s", pluralNoun);
-    	printf("\nEnter a celebrity (first and last names): ");
-    	scanf("%s%s", celebrityF, celebrityL);
+    printf("\nEnter a color: ");
+	scanf("%s", color);
+    printf("\nEnter a plural noun: ");
+    scanf("%s", pluralNoun);
+    printf("\nEnter a celebrity (first and last names): ");
+    scanf("%s%s", celebrityF, celebrityL);
     
-       	// Separator
-	    printf("\n-------------------------------------------\n");	
+    // Separator
+	printf("\n-------------------------------------------\n");	
 
-    	printf("Roses are %s\n", color);
-    	printf("%s are blue\n", pluralNoun);
-    	// Note the two "%s", one for first and last names
-    	printf("I love %s %s\n\n", celebrityF, celebrityL);
+    printf("Roses are %s\n", color);
+    printf("%s are blue\n", pluralNoun);
+    // Note the two "%s", one for first and last names
+    printf("I love %s %s\n\n", celebrityF, celebrityL);
     
-    	return 0;
+    return 0;
 }
